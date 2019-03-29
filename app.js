2541230fbc50a98d6904abc4c76a5f100cb36c55
Chittrang Mishra <chittrang.m@gmail.com>
2019-03-29 12:13:07 +0530

    method params corrected

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use((req, res, next)=>{
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     if(req.method == "OPTIONS"){
-        res.header("Access-Contol-Allow-Methods", "*");
+        res.header("Access-Contol-Allow-Methods", 'PUT', 'PATCH', 'DELETE', 'GET', 'POST');
         return res.status(200).json({});
     }
     next();
