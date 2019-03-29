ad1f20d2b80f971923c48912e2830e6428020d61
Chittrang Mishra <chittrang.m@gmail.com>
2019-03-29 12:22:04 +0530

    allowed all methods

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use((req, res, next)=>{
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     if(req.method === "OPTIONS"){
-        res.header("Access-Control-Allow-Methods", 'PUT', 'PATCH', 'DELETE', 'GET', 'POST');
+        res.header("Access-Control-Allow-Methods", 'PUT, PATCH, DELETE, GET, POST');
         return res.status(200).json({});
     }
     next();
