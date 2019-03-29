20fb282025a8aaf729efa87a18173f8fc7f04c96
Chittrang Mishra <chittrang.m@gmail.com>
2019-03-29 12:19:38 +0530

    spell correction in allow methods

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.use(bodyParser.json());
 app.use((req, res, next)=>{
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if(req.method == "OPTIONS"){
-        res.header("Access-Contol-Allow-Methods", 'PUT', 'PATCH', 'DELETE', 'GET', 'POST');
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods", 'PUT', 'PATCH', 'DELETE', 'GET', 'POST');
         return res.status(200).json({});
     }
     next();
